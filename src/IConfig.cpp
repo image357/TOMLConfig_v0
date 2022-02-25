6a77cc658364551b8325879e31f2909a6bee7950
Marcel Köpke <marcel.koepke@gmail.com>
2022-02-26 00:30:17 +0100

    basic setup

---
+++ src/IConfig.cpp
@@ -0,0 +1,9 @@
+//
+// Created by koepke on 2/26/22.
+//
+
+#include "IConfig.h"
+
+namespace tomlc {
+
+}
---
+++ src/IConfig.h
@@ -0,0 +1,16 @@
+//
+// Created by koepke on 2/26/22.
+//
+
+#ifndef TOMLCONFIG_ICONFIG_H
+#define TOMLCONFIG_ICONFIG_H
+
+namespace tomlc {
+
+class IConfig {
+
+};
+
+}
+
+#endif //TOMLCONFIG_ICONFIG_H
