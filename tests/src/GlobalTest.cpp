0341cdc7bbfaba7b04c4bdd6ead6ba939d0d00ae
Marcel Köpke <marcel.koepke@gmail.com>
2022-03-12 22:06:47 +0100

    add virtual destructor test

--- tests/src/GlobalTest.cpp
+++ tests/src/GlobalTest.cpp
@@ -20,3 +20,47 @@ TEST_F(GlobalTest, succeed)
 {
     SUCCEED();
 }
+
+TEST_F(GlobalTest, virtualDestructorChain)
+{
+    int i = 0;
+    class First {
+    public:
+        First(int& ref)
+                :ref(ref) { ++ref; }
+        virtual ~First() { --ref; }
+        int& ref;
+    };
+    {
+        i = 0;
+        std::unique_ptr<First> first = std::make_unique<First>(i);
+        ASSERT_EQ(i, 1);
+    }
+    ASSERT_EQ(i, 0);
+
+    class Second : public First {
+    public:
+        Second(int& ref)
+                :First(ref) { ++ref; }
+//        ~Second() { --ref; }
+    };
+    {
+        i = 0;
+        std::unique_ptr<First> second = std::make_unique<Second>(i);
+        ASSERT_EQ(i, 2);
+    }
+    ASSERT_EQ(i, 1);
+
+    class Third : public Second {
+    public:
+        Third(int& ref)
+                :Second(ref) { ++ref; }
+        ~Third() { --ref; }
+    };
+    {
+        i = 0;
+        std::unique_ptr<First> third = std::make_unique<Third>(i);
+        ASSERT_EQ(i, 3);
+    }
+    ASSERT_EQ(i, 1);
+}
