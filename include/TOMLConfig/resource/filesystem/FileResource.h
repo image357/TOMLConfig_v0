b36a3a8dd20bbc1c5469d045b36682462585efd9
Marcel Köpke <marcel.koepke@gmail.com>
2022-03-12 21:29:36 +0100

    create FileResource

---
+++ include/TOMLConfig/resource/filesystem/FileResource.h
@@ -0,0 +1,18 @@
+//
+// Created by koepke on 3/12/22.
+//
+
+#ifndef TOMLCONFIG_FILERESOURCE_H
+#define TOMLCONFIG_FILERESOURCE_H
+
+#include "TOMLConfig/resource/abstract/LeafResource.h"
+
+namespace config::resource {
+
+class FileResource : public LeafResource {
+
+};
+
+}
+
+#endif //TOMLCONFIG_FILERESOURCE_H
---
+++ src/resource/filesystem/FileResource.cpp
@@ -0,0 +1,9 @@
+//
+// Created by koepke on 3/12/22.
+//
+
+#include "TOMLConfig/resource/filesystem/FileResource.h"
+
+namespace config::resource {
+
+}
