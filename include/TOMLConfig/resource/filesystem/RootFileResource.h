ea899d60f464bee55e3ac90db7359dc2c212f501
Marcel Köpke <marcel.koepke@gmail.com>
2022-03-20 11:36:44 +0100

    add RootFileResource

---
+++ include/TOMLConfig/resource/filesystem/RootFileResource.h
@@ -0,0 +1,16 @@
+//
+// Created by koepke on 3/20/22.
+//
+
+#ifndef TOMLCONFIG_ROOTFILERESOURCE_H
+#define TOMLCONFIG_ROOTFILERESOURCE_H
+
+namespace config::resource {
+
+class RootFileResource {
+
+};
+
+}
+
+#endif //TOMLCONFIG_ROOTFILERESOURCE_H
---
+++ src/resource/filesystem/RootFileResource.cpp
@@ -0,0 +1,9 @@
+//
+// Created by koepke on 3/20/22.
+//
+
+#include "TOMLConfig/resource/filesystem/RootFileResource.h"
+
+namespace config::resource {
+
+}
